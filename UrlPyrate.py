26c2852a8967dec3207c1396668aacf6aa6dfc8e
rob22202 <rob22202@gmail.com>
2013-05-31 12:24:24 -0300

    Update UrlPyrate.py

--- UrlPyrate.py
+++ UrlPyrate.py
@@ -4,35 +4,26 @@
 # UrlPyrate (aka PerfectGirlfriend) 200s every/any HTTP request, serving default content.  
 # Use with a DNS blackhole for funtimes.
 
-# This script was created by rob22202@gmail.com @rob22202 
-# and inspired by Fakenet http://practicalmalwareanalysis.com/fakenet/
+# This script was created by rob22202@gmail.com @rob22202 # and inspired by Fakenet http://practicalmalwareanalysis.com/fakenet/
 
-# This script is not intended to be use don the internet, as it is 
-# untested for vulnerabilities.  Caveat Emptor.
+# This script is not intended to be use don the internet, as it is # untested for vulnerabilities.  Caveat Emptor.
 
 # Please feel free to use and improve.
 
-# Url Pyrate is designed to be used with DNS manipulation.  Redirect your favorite 
-# malicious domain to the server running UrlPyrate via a DNS blackhole. Then, 
-# sit back and watch the requests being made to that domain.  Extra fun is 
-# provided by looking at the referrer field.  That will show you what site linked 
-# to the malicious domain directly or via a hidden embedded iframe.  Have fun.
+# Url Pyrate is designed to be used with DNS manipulation.  Redirect your favorite # malicious domain to the server running UrlPyrate via a DNS blackhole. Then, # sit back and watch the requests being made to that domain.  Extra fun is # provided by looking at the referrer field.  That will show you what site linked # to the malicious domain directly or via a hidden embedded iframe.  Have fun.
 
 # UrlPyrate listens with a simple web server on ports 80 and 443, by default.
 
-# UrlPyrate will return a 200 to any request it is given, by default it will always 
-# return the contents of 1.html to the requestor.  Any file placed in the running 
-# directory of the app with the name "1" ,followed by an appropriate extension will 
-# be served if a file with that same extension is requested.
+# UrlPyrate will return a 200 to any request it is given, by default it will always # return the contents of 1.html to the requestor.  Any file placed in the running # directory of the app with the name "1" ,followed by an appropriate extension will # be served if a file with that same extension is requested.
 
-# At a minimum, This script requires that a SSL certificate named server.pem and a 
-# default content file named 1.html exist in the same directory from whish it is run.
+# At a minimum, This script requires that a SSL certificate named server.pem and a # default content file named 1.html exist in the same directory from whish it is run.
 
 # SSL certificate creation:  openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 
 # UrlPyrate ouputs pipe delimited logs to UrlPyrate.log, in directory from which it was run.
 
-import SimpleHTTPServer, os, BaseHTTPServer, ssl, sys, time from multiprocessing import Process
+import SimpleHTTPServer, os, BaseHTTPServer, ssl, sys, time 
+from multiprocessing import Process
 
 logfile = 'UrlPyrate.log'
 
@@ -64,15 +55,19 @@ def run_https_server(https_port):
 				server_side=True )
 		httpd.serve_forever()
 	except SSLError:
-		print 'SSL Error'
+		print 'HTTPS Error'
 		pass
 		
 def run_http_server(http_port):
 	print "Serving HTTP on port %d" % (http_port)
-	httpd = BaseHTTPServer.HTTPServer(
-			('', http_port),
-			SimplerHTTPRequestHandler )
-	httpd.serve_forever()
+	try
+		httpd = BaseHTTPServer.HTTPServer(
+				('', http_port),
+				SimplerHTTPRequestHandler )
+		httpd.serve_forever()
+	except:
+		print 'HTTP Error'
+		pass
 
 class SimplerHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def send_head(self):
@@ -108,7 +103,6 @@ class SimplerHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			mode = 'r'
 		else:
 			mode = 'rb'
-
 		try:
 			f = open(path, mode)
 		except IOError:
@@ -122,3 +116,4 @@ class SimplerHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
 if __name__ == "__main__":
 	main()
+
